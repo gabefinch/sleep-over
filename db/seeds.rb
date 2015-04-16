923ac4cc8e8a42056ca946e2031e01ecc38ee1d8
Gabe Finch <gabefinch@gmail.com>
2015-04-16 15:01:21 -0700

    remove images from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,26 +11,7 @@
 
 users = User.create!([{email: 'rogerwhite@awesome.com', password: '12345678'},
                     {email: 'juniperruby@awesome.com', password: '12345678'}])
-
-
-
 space1 = users.first.spaces.create!({description: "A place where you can go and take off your hat.",
                             address: "Dream Forest", cost: "112"})
-
-
-
-
-
-photos1 = users.first.spaces.first.photos.create!([{image: "app/assets/images/cabin1_1.jpg"},
-                                 {image: "app/assets/images/cabin1_2.jpg"},
-                                 {image: "app/assets/images/cabin1_3.jpg"}])
-
-
 users[1].spaces.create!([{description: "A lovely remote retreat away from it all",
                             address: "Columbia Gorge", cost: "96"}])
-
-
-users[1].spaces.first.photos.create!([{image: "app/assets/images/cabin2_1.jpg"},
-                                 {image: "app/assets/images/cabin2_2.jpg"},
-                                 {image: "app/assets/images/cabin2_3.jpg"},
-                                 {image: "app/assets/images/cabin2_4.jpg"}])
